c4ecba50d4dd1bf85de919dbb8fc08a216ceaf17
Fergal Moylan <fergalmoylan@MacBook-Air.station>
2024-11-26 12:07:53 +0000

    Set up project

---
+++ src/bluesky_post_aggregator/core.clj
@@ -0,0 +1,6 @@
+(ns bluesky-post-aggregator.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/bluesky_post_aggregator/core_test.clj
@@ -0,0 +1,7 @@
+(ns bluesky-post-aggregator.core-test
+  (:require [clojure.test :refer :all]
+            [bluesky-post-aggregator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
