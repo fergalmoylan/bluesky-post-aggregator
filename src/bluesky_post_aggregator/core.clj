c8860e9cbb68d365c2eb18734c53c7fdfbba3dfa
Fergal Moylan <fergalmoylan@MacBook-Air.station>
2024-11-26 18:33:18 +0000

    Implement Kafka Streams Topology

--- src/bluesky_post_aggregator/core.clj
+++ src/bluesky_post_aggregator/core.clj
@@ -1,7 +1,9 @@
 (ns bluesky-post-aggregator.core
   (:require [environ.core :refer [env]]
             [jackdaw.serdes.json :as jsonserde]
+            [jackdaw.streams :as js]
             [clojure.tools.logging :as log]
+            [bluesky-post-aggregator.topology :refer [topology-builder]]
             [clojure.pprint :refer [pprint]]))
 
 (defn -main
@@ -12,14 +14,19 @@
                     "cache.max.bytes.buffering" "100000"
                     "topic-config" {:input {:topic-name (env :input-topic)
                                             :partition-count (env :input-topic-partitions)
-                                            :replication-factor (env :input-topic-replication-factor)
+                                            :replication-factor 1
                                             :key-serde (jsonserde/serde)
                                             :value-serde (jsonserde/serde)}
                                     :output {:topic-name (env :output-topic)
                                              :partition-count (env :output-topic-partitions)
-                                             :replication-factor (env :output-topic-replication-factor)
+                                             :replication-factor 1
                                              :key-serde (jsonserde/serde)
-                                             :value-serde (jsonserde/serde)}}}]
+                                             :value-serde (jsonserde/serde)}}}
+        streams-builder (js/streams-builder)
+        topology (topology-builder streams-builder app-config)
+        app (js/kafka-streams topology app-config)]
     (log/info "Starting application with config:\n"
               (with-out-str
-                (pprint app-config)))))
+                (pprint app-config)))
+    (js/start app)
+    app))
