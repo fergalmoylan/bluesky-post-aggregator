7913890e87d19be6b39250ab000c229fa7a0a287
Fergal Moylan <fergalmoylan@MacBook-Air.station>
2024-11-26 13:06:16 +0000

    Set up app configuration with env variables

--- src/bluesky_post_aggregator/core.clj
+++ src/bluesky_post_aggregator/core.clj
@@ -1,6 +1,23 @@
-(ns bluesky-post-aggregator.core)
+(ns bluesky-post-aggregator.core
+  (:require [environ.core :refer [env]]
+            [jackdaw.serdes.json :as jsonserde]
+            [clojure.tools.logging :as log]
+            [clojure.pprint :refer [pprint]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (let [app-config {"bootstrap.servers" (env :kafka-addresses)
+                    "application.id" (env :application-id)
+                    "auto.offset.reset" "earliest"
+                    "cache.max.bytes.buffering" "100000"
+                    "topic-config" {:input {:topic-name (env :input-topic)
+                                            :partition-count (env :input-topic-partitions)
+                                            :replication-factor (env :input-topic-replication-factor)
+                                            :key-serde (jsonserde/serde)
+                                            :value-serde (jsonserde/serde)}
+                                    :output {:topic-name (env :output-topic)
+                                             :partition-count (env :output-topic-partitions)
+                                             :replication-factor (env :output-topic-replication-factor)
+                                             :key-serde (jsonserde/serde)
+                                             :value-serde (jsonserde/serde)}}}]
+    (log/info "Starting application with config:\n" (with-out-str (pprint app-config)))))
